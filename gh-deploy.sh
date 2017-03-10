3660cc2c988291a68b6faf8d44edc4fd5b5fad5b
fwindpeak <fwindpeak@gmail.com>
2017-03-10 11:59:45 +0800

    add replace

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -5,6 +5,7 @@ cd gh-pages
 git checkout -B gh-pages
 rm -rf *
 cp -r ../dist/* .
+sed -i "s/=\/static\//=.\/static\//g" index.html
 git add .
 git commit -m "deploy $DATE"
 git push -f origin gh-pages:gh-pages
